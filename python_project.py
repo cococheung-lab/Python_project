df23be0502d84359bf9a0539b58f673fdd8c63f8
cococheung-lab <cococheung1203@gmail.com>
2023-12-02 03:40:00 +0800

    Delete python_project.py

--- python_project.py
+++
@@ -1,8 +0,0 @@
-import forex_python.converter as c
-
-# Get the listed currency on forex_python
-listed_currency = c.get_rates('').keys()
-
-# Check if the source currency is valid
-while True:
-        source_currency = input("Enter the source currency code (e.g., AUD, HKD): ").upper()
